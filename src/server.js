2d6d503bba49bbadeaf807f5946428142b9371b1
selbekk <kristofer.selbekk@bekk.no>
2015-08-24 19:12:11 +0200

    Set up basic Express server and some basic routes

--- src/server.js
+++ src/server.js
@@ -1,1 +1,20 @@
-module.exports = {};
+let express = require('express');
+let express = require('express-handlebars');
+let app = express();
+
+app.engine('handlebars', exphbs({ defaultLayout: 'main', layoutsDir: __dirname + '/views/layouts' }));
+app.set('view engine', 'handlebars');
+app.set('views', __dirname + '/views');
+app.set('port', process.env.PORT || 4000);
+
+// Middleware
+app.use('/assets', express.static('dist'));
+
+// Routes
+app.get('/', (req, res) => res.render('index'));
+app.get('/privacy', (req, res) => res.render('privacy'));
+
+// Start server
+app.listen(app.get('port'),
+    () => console.log('Server started at port ' + app.get('port') + '.');
+);
